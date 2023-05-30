72d8a295957d4f9ac3b1ffd16f89e88b4d907f83
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2023-05-30 19:45:52 -0300

    update

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,52 +1,81 @@
-const navigate = (docID) => {
-    const url = window.location.href.substring(0, window.location.href.lastIndexOf("/"));
-    const path = `${url}/document.html?id=${docID}`
-    window.location.assign(path)
-}
+const url = location.href.substring(0, location.href.lastIndexOf("/"));
+const imgPath = `${url}/assets/img/docs`;
+
+const navigate = (id) => {
+    const path = `${url}/document.html?id=${id}`;
+    location.assign(path);
+};
+
+const createCard = (id) => {
+    const card = document.createElement("div");
+    card.classList.add("col-xxl-4", "col-md-6", "d-flex", "justify-content-center");
+    card.onclick = function() {
+        navigate(id);
+    };
+
+    return card;
+};
+
+const createInnerCard = () => {
+    const innerCard = document.createElement("div");
+    innerCard.classList.add("card", "h-100", "myCardStyle");
+    innerCard.style.width = "18rem";
+
+    return innerCard;
+};
+
+const createCardBody = () => {
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    return cardBody;
+};
+
+const createCardImg = (id, url, name) => {
+    const image = document.createElement("img");
+    image.classList.add("card-img-top", "img-fluid");
+    image.style.cssText = `
+        width: 100%;
+        height: 175px;
+    `;
+    image.src = `${imgPath}/320/image${id}.avif` || url;
+    image.alt = `${name} image`;
+    if (id > 2) {
+        image.loading = "lazy";
+    }
+
+    return image;
+};
+
+const createCardText = (name) => {
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.textContent = name;
+
+    return cardText;
+};
 
 async function renderCards() {
     try {
         const res = await fetch("assets/json/docs.json");
         const docs = await res.json();
 
-        const cardsContainer = window.document.querySelector("#cardsContainer");
-        const url = window.location.href.substring(0, window.location.href.lastIndexOf("/"));
-        const imgPath = `${url}/assets/img/docs`;
+        const cardsContainer = document.querySelector("#cardsContainer");
 
         for (let index = 0; index < docs.length; index++) {
-            const doc = docs[index];
+        const { id, name, url, texts } = docs[index];
 
-            const card = document.createElement("div");
-            card.classList.add("col-xxl-4", "col-md-6", "d-flex", "justify-content-center");
-            card.onclick = function() {
-                navigate(doc.id);
-            };
+        const card = createCard(id);
+        const innerCard = createInnerCard();
+        const image = createCardImg(id, url, name);
+        const cardBody = createCardBody();
+        const cardText = createCardText(name);
 
-            const innerCard = document.createElement("div");
-            innerCard.classList.add("card", "h-100", "myCardStyle");
-            innerCard.style.width = "18rem";
-
-            const image = document.createElement("img");
-            image.classList.add("card-img-top", "img-fluid");
-            image.style.width = "100%";
-            image.style.height = "175px";
-            image.src = `${imgPath}/320/image${doc.id}.avif`;
-            if(!image.src) image.src = doc.url
-            image.alt = `${doc.name} image`;
-            if(index >= 2) image.loading = "lazy";
-
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-
-            const cardText = document.createElement("p");
-            cardText.classList.add("card-text");
-            cardText.textContent = doc.name;
-
-            cardBody.appendChild(cardText);
-            innerCard.appendChild(image);
-            innerCard.appendChild(cardBody);
-            card.appendChild(innerCard);
-            cardsContainer.appendChild(card);
+        cardBody.append(cardText);
+        innerCard.append(image);
+        innerCard.append(cardBody);
+        card.append(innerCard);
+        cardsContainer.append(card);
         }
     } catch (err) {
         console.error(err);
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,61 +1,95 @@
-const url = window.location.href.substring(0, window.location.href.lastIndexOf("/"));
+const url = location.href.substring(0, location.href.lastIndexOf("/"));
+const imgPath = `${url}/assets/img/products`;
 
-const navigate = (docID) => {
-    const docPath = `${url}/product.html?id=${docID}`
-    window.location.assign(docPath)
-}
+const navigate = (id) => {
+  const docPath = `${url}/product.html?id=${id}`;
+  location.assign(docPath);
+};
+
+const createCard = (id) => {
+  const card = document.createElement("div");
+  card.classList.add("col-xxl-3", "col-md-4", "d-flex", "justify-content-center");
+  card.onclick = function() {
+    navigate(id);
+  };
+
+  return card;
+};
+
+const createInnerCard = () => {
+  const innerCard = document.createElement("div");
+  innerCard.classList.add("card", "h-100", "myCardStyle");
+  innerCard.style.width = "14rem";
+
+  return innerCard;
+};
+
+const createCardImg = (id, url, name) => {
+  const image = document.createElement("img");
+  image.classList.add("card-img-top", "img-fluid");
+  image.style.cssText = `
+    width: 222px;
+    height: 225px;
+  `;
+  image.src = `${imgPath}/image${id}.avif` || url;
+  image.alt = `${name} image`;
+  if (id > 2) {
+    image.loading = "lazy";
+  }
+
+  return image;
+};
+
+const createCardBody = () => {
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  return cardBody;
+};
+
+const createPriceElement = (price) => {
+  const priceElement = document.createElement("h2");
+  priceElement.classList.add("card-text");
+  priceElement.textContent = `R$${price.toFixed(2).replace(".", ",")}`;
+
+  return priceElement;
+};
+
+const createSummaryElement = (summary) => {
+  const summaryElement = document.createElement("p");
+  summaryElement.classList.add("card-text", "fs-6", "fw-normal");
+  summaryElement.textContent = summary;
+
+  return summaryElement;
+};
 
 async function renderCards() {
-    try {
-        const res = await fetch("assets/json/products.json");
-        const products = await res.json();
+  try {
+    const res = await fetch("assets/json/products.json");
+    const products = await res.json();
 
-        const cardsContainer = window.document.querySelector("#cardsContainer");
-        const imgPath = `${url}/assets/img/products`;
+    const cardsContainer = document.querySelector("#cardsContainer");
 
-        for (let index = 0; index < products.length; index++) {
-            const product = products[index];
+    for (let index = 0; index < products.length; index++) {
+      const { id, price, name, summary } = products[index];
 
-            const card = document.createElement("div");
-            card.classList.add("col-xxl-3", "col-md-4", "d-flex", "justify-content-center");
-            card.onclick = function() {
-                navigate(product.id);
-            };
+      const card = createCard(id);
+      const cardBody = createCardBody();
+      const image = createCardImg(id, url, name);
+      const innerCard = createInnerCard();
+      const priceElement = createPriceElement(price);
+      const summaryElement = createSummaryElement(summary);
 
-            const innerCard = document.createElement("div");
-            innerCard.classList.add("card", "h-100", "myCardStyle");
-            innerCard.style.width = "14rem";
-
-            const image = document.createElement("img");
-            image.classList.add("card-img-top", "img-fluid");
-            image.style.width = "100%";
-            image.style.height = "220px";
-            image.src = `${imgPath}/image${product.id}.avif`;
-            if(!image.src) image.src = product.url
-            image.alt = `${product.name} image`;
-            if(index >= 2) image.loading = "lazy";
-
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-
-            const cardPrice = document.createElement("h2");
-            cardPrice.classList.add("card-text");
-            cardPrice.textContent = `R$${product.price}`.replace(".",",");
-
-            const cardSummary = document.createElement("p");
-            cardSummary.classList.add("card-text","fs-6", "fw-normal");
-            cardSummary.textContent = product.summary
-
-            cardBody.appendChild(cardPrice);
-            cardBody.appendChild(cardSummary);
-            innerCard.appendChild(image);
-            innerCard.appendChild(cardBody);
-            card.appendChild(innerCard);
-            cardsContainer.appendChild(card);
-        }
-    } catch (err) {
-        console.error(err);
+      cardBody.append(priceElement);
+      cardBody.append(summaryElement);
+      innerCard.append(image);
+      innerCard.append(cardBody);
+      card.append(innerCard);
+      cardsContainer.append(card);
     }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 window.addEventListener("load", renderCards);
